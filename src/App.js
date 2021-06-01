82093c4e3aff458454a4f1b0855e0cebb3b691ec
NordicMichael <nordicmichael@gmail.com>
2021-06-01 13:19:09 +0300

    'asdasd'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { useDispatch } from 'react-redux';
+import {increment} from './features/counter/counterSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://upload.wikimedia.org/wikipedia/commons/e/e5/Crystal_Project_cookie.png' 
+        onClick={() => dispatch(increment())}
+        alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
